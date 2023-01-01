4fbca6a1dd9f0fe36b58048c43bf041690dcdf69
Narik <narikgame1@gmail.com>
2023-01-01 22:22:35 +1100

    Completed the day 1 challenge plus the bonus challenges

--- Daily/Day1/Program.cs
+++ Daily/Day1/Program.cs
@@ -1,1 +1,39 @@
-﻿
+﻿// This could also be used to find the number with the plurality, i.e., the mode
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+int MaxElements = 50000; 
+
+List<int> numbers = new() {
+	3, 1, 1, 3, 1, 5, 2, 1, 6, 9, 8, 1, 1, 2, 1, 1
+};
+
+// This is likely space inefficient, but satisfies O(1) space complexity.
+// This could be slightly easier to manage, in a sense, if these were arrays of tuples.
+// Given that the target number is in the majority, there can be no more than MaxElements/2 unique values.
+int[] elementValues = new int[MaxElements / 2];
+int[] elementCounts = new int[MaxElements / 2];
+
+int position = -1; // Keep track of how much of the array has been filled
+
+// Figure out what numbers are in the list
+foreach (int num in numbers) {
+	if (!elementValues.Contains(num)) {
+		elementValues[++position] = num;
+	}
+}
+
+position = -1; // Use this to keep track of what index in the values array corresponds with the index in the counts array
+
+// Get a count of each number that does occur in the list
+foreach (int num in elementValues) {
+	elementCounts[++position] = numbers.Count(x => x == num);
+}
+
+// Find the number that had the highest occurance and get it's position in the array
+int result = elementCounts.Max();
+position = Array.IndexOf(elementCounts, result);
+
+Console.WriteLine("The majority number is: " + elementValues[position]);
