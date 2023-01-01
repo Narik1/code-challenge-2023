d0fff9898b7a7a6864eb011a6584c67ad19a739b
Narik <narikgame1@gmail.com>
2023-01-01 13:05:02 +1100

    Added and started Week1 project

---
+++ Weekly/Week1/Palindrome.cs
@@ -0,0 +1,10 @@
+﻿namespace Narik.Code.Weekly;
+
+public sealed class Palindrome
+{
+	public string Input { get; private set; }
+
+	public Palindrome(System.IO.TextReader inputReader) {
+		Input = inputReader.ReadToEnd();
+	}
+}
