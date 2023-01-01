9452c8b1e5f563a0eb6ca100ba003c70197b0496
Narik <narikgame1@gmail.com>
2023-01-01 15:41:04 +1100

    Completed first attempt at week 1 challenge

--- Weekly/Week1/Palindrome.cs
+++ Weekly/Week1/Palindrome.cs
@@ -1,10 +1,62 @@
-﻿namespace Narik.Code.Weekly;
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Narik.Code.Weekly.Week1;
 
 public sealed class Palindrome
 {
+	private readonly bool _caseMatters;
+
 	public string Input { get; private set; }
 
-	public Palindrome(System.IO.TextReader inputReader) {
-		Input = inputReader.ReadToEnd();
+	private string? _palindrome;
+	public string Output {
+		get {
+			if (_palindrome is null) {
+				CalculatePalindrome();
+			}
+
+			return _palindrome;
+		}
+	}
+
+
+	public Palindrome(System.IO.TextReader inputReader, bool caseMatters = false) {
+		Input = inputReader.ReadLine() ?? String.Empty;
+
+		_caseMatters = caseMatters;
+	}
+
+	[MemberNotNull(nameof(_palindrome))]
+	private void CalculatePalindrome() {
+		_palindrome = String.Empty;
+
+		int newLength = 1;
+		do {
+			for (int i = 0; i < Input.Length - newLength; i++) {
+				string testCase = Input[i..(i + newLength)];
+				string reverseCase = Reverse(testCase);
+
+				if (_caseMatters && testCase == reverseCase) {
+					_palindrome = testCase;
+					break;
+				} else if (!_caseMatters && testCase.ToLowerInvariant() == reverseCase.ToLowerInvariant()) {
+					_palindrome = testCase;
+					break;
+				}
+			}
+		} while (++newLength <= Input.Length);
+	}
+
+	/// <summary>
+	/// Reverses a string on a char basis.
+	/// Only works in Unicode BMP due to char being UTF-16.
+	/// </summary>
+	/// <param name="input">The string to be reversed</param>
+	/// <returns>The reverse string of the input</returns>
+	private static string Reverse(string input) {
+		char[] temp = input.ToCharArray();
+		Array.Reverse(temp);
+		return new(temp);
 	}
 }
---
+++ Weekly/Week1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace Narik.Code.Weekly.Week1;
+
+internal class Program
+{
+	public static void Main(string[] args) {
+		Palindrome palindrome;
+
+		if (args.Length > 0) {
+			string input = String.Empty;
+			for (int i = 0; i < args.Length; i++) {
+				input += args[i];
+			}
+
+			StringReader reader = new(input);
+			palindrome = new(reader);
+		} else {
+			Console.WriteLine("Provide string to find palindrome within:");
+			palindrome = new(Console.In);
+		}
+
+		Console.WriteLine($"Input: {palindrome.Input}");
+		Console.WriteLine($"Output: {palindrome.Output}");
+	}
+}
