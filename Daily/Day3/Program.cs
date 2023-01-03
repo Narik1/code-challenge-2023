cb4b2ce59cae9a531b80f7916ed808be2e635154
Narik <narikgame1@gmail.com>
2023-01-03 21:56:26 +1100

    Added option for performing HTTP GET request via HttpClient

--- Daily/Day3/Program.cs
+++ Daily/Day3/Program.cs
@@ -14,9 +14,11 @@
  */
 
 using System;
+using System.Net.Http;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading;
+using System.Threading.Tasks;
 
 // Setup variables
 Uri website = new("http://example.com/");
@@ -30,27 +32,40 @@ Connection: close
 
 // Configuring all these awaits isn't necessary in this project, but whatever
 
-// Establish socket connection
-using Socket socket = new(SocketType.Stream, ProtocolType.Tcp);
-await socket.ConnectAsync(website.Host, website.Port, token).ConfigureAwait(false);
+async ValueTask<string> SocketRequestAsync() {
+	// Establish socket connection
+	using Socket socket = new(SocketType.Stream, ProtocolType.Tcp);
+	await socket.ConnectAsync(website.Host, website.Port, token).ConfigureAwait(false);
 
-// Send request
-int bytesSent = 0;
-while (bytesSent < requestBytes.Length) {
-	bytesSent += await socket.SendAsync(requestBytes, SocketFlags.None, token).ConfigureAwait(false);
-}
-
-// Retreive response
-while (true) {
-	byte[] buffer = new byte[256]; // Allocating a new array every loop? Gross.
-	int bytesReceived = await socket.ReceiveAsync(buffer, SocketFlags.None, token).ConfigureAwait(false);
-
-	await Console.Out.WriteAsync(Encoding.ASCII.GetString(buffer)).ConfigureAwait(false);
-
-	if (bytesReceived < buffer.Length - 1) { // Assume end of response
-		break;
+	// Send request
+	int bytesSent = 0;
+	while (bytesSent < requestBytes.Length) {
+		bytesSent += await socket.SendAsync(requestBytes, SocketFlags.None, token).ConfigureAwait(false);
 	}
+
+	StringBuilder builder = new();
+	// Retreive response
+	while (true) {
+		byte[] buffer = new byte[256]; // Allocating a new array every loop? Gross.
+		int bytesReceived = await socket.ReceiveAsync(buffer, SocketFlags.None, token).ConfigureAwait(false);
+
+		builder.Append(Encoding.ASCII.GetString(buffer));
+
+		if (bytesReceived < buffer.Length - 1) { // Assume end of response
+			break;
+		}
+	}
+
+	// Disconnect, maybe unnecessary
+	await socket.DisconnectAsync(false, token).ConfigureAwait(false);
+
+	return builder.ToString();
 }
 
-// Disconnect, maybe unnecessary
-await socket.DisconnectAsync(false, token).ConfigureAwait(false);
+async ValueTask<string> HttpRequestAsync() {
+	using HttpClient client = new();
+	return await client.GetStringAsync(website.AbsoluteUri, token).ConfigureAwait(false);
+}
+
+//Console.WriteLine(await HttpRequestAsync().ConfigureAwait(false));
+Console.WriteLine(await SocketRequestAsync().ConfigureAwait(false));
