cb942ed88dbf3ac237deefab8880b5f70fedd02d
Kiran Bommakanti <bommakantikiran@gmail.com>
2023-02-10 03:04:46 +1100

    Added challenge description into the code for day 4

--- Daily/Day4/Program.cs
+++ Daily/Day4/Program.cs
@@ -1,4 +1,14 @@
-﻿using System;
+﻿/*
+ * DAY 4  •  Neural Intercourse 
+ * Today's challenge is to write an interpreter for the brainfuck esoteric language.
+ *
+ * Research how this language works and what each instruction does (>,<,[,],+,-,.). Then use this information to make an interpreter that reads a brainfuck program from standard input and displays the result.
+ *
+ * Goblin mode:
+ * Use the brainfuck interpreter you wrote to write a brainfuck interpreter.
+ */
+
+using System;
 using System.IO;
 
 using Narik.CodeChallenge.Day4;
